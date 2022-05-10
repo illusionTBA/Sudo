f59595d728a25fdb28ca3742f324280cdd790a68
Avad3 <jamesonmartin49@gmail.com>
2022-05-09 22:20:19 -0400

    hi

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import { fastify } from 'fastify';
-import fastifyStatic from 'fastify-static';
+import fastifyStatic from '@fastify/static';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import Server from 'bare-server-node';
--- static/js/commands.js
+++ static/js/commands.js
@@ -6,75 +6,74 @@
 - Very ass coding here like i mean REALLY ASS. i will improve later on in development.
 
 */
+const el = document.getElementById('terminal');
+const move = document.getElementById('terminal__bar');
+const container = document.getElementById('container');
+const drag = new Draggabilly(el, {
+    containment: container,
+    handle: move
+});
+let currentDir = '/home/user/Desktop/';
 
-
-
-var termInput = document.querySelector(".termInput");
-var output = document.querySelector(".termOutput");
-termInput.addEventListener("keyup", function (event) {
+var termInput = document.querySelector('.termInput');
+var output = document.querySelector('.termOutput');
+termInput.addEventListener('keyup', (event) => {
     if (event.code === 'Enter') {
-        if (termInput.value == "ls") {
-            console.log("ls");
-            output.innerHTML = "";
-            output.innerHTML += "Games/<br>Proxy/<br>Browser/<br>";
-            termInput.value = "";
-        } else if (termInput.value == "pwd") {
-            console.log("pwd");
-            output.innerHTML = "";
-            output.innerHTML += "/home/user/Desktop/";
-            termInput.value = "";
-
-        } else if (termInput.value == "cat") {
-            console.log("cat");
-            output.innerHTML = "";
-            output.innerHTML += "cat: cat: No such file or directory<br>";
-            termInput.value = "";
-
-        } else if (termInput.value == "clear") {
-            output.innerHTML = "";
-            termInput.value = "";
-        } else if (termInput.value == "help") {
-            output.innerHTML = "ls - list files in current directory<br>pwd - print current directory<br>cat - concatenate files and print on the standard output<br>clear - clear the terminal<br>help - print this message";
-            termInput.value = "";
-
-        } // else if terminput = "cd {path}"
-        else if (termInput.value.includes("cd")) {
-            output.innerHTML = "";
-            output.innerHTML += "cd: no such file or directory: " + termInput.value.substring(3) + "<br>";
-            termInput.value = "";
-        } // else if input = "proxy {url}" send them to a url
-        else if (termInput.value.includes("proxy")) {
-            if (termInput.value.includes("https://")) {
-                output.innerHTML = termInput.value.substring(8);
-            } else if (termInput.value.includes("http://")) {
-                var url = termInput.value.substring(8);
-                output.innerHTML = url;
-            } else {
-                output.innerHTML = "";
-                output.innerHTML += "proxy: invalid url: " + termInput.value.substring(7) + "<br>";
-                termInput.value = "";
-            }
-        } else if (termInput.value == "neofetch") {
-            output.innerHTML = "";
-            output.innerHTML += ` <pre>
-            ██████████████████  ████████   Sudo@illusions
-            ██████████████████  ████████   -------------------
-            ██████████████████  ████████   OS: Cumjaro Linux x86_64
-            ██████████████████  ████████   Kernel: 5.10.30-1-MANCUMJARO
-            ████████            ████████   Uptime: 1 day, 23 hours, 38 minutes, and 36 seconds
-            ████████  ████████  ████████   Packages: to much(pacman)
-            ████████  ████████  ████████   Shell: bash 5.1.8
-            ████████  ████████  ████████   Terminal: cumsole
-            ████████  ████████  ████████   CPU: shit cum k590-k @ idc@ghz
-            ████████  ████████  ████████   Memory: 16.5G/32G
-            ████████  ████████  ████████
-            ████████  ████████  ████████
-            ████████  ████████  ████████
-            ████████  ████████  ████████
-            </pre>`;
-        } else {
-            output.innerHTML = "";
-            output.innerHTML += "bash: " + termInput.value + ": command not found<br>";
+        switch (termInput.value) {
+            case 'ls':
+                console.log('ls');
+                output.innerHTML = '';
+                output.innerHTML += 'Games<br>Proxy<br>';
+                termInput.value = '';
+                break;
+            case 'pwd':
+                console.log('pwd');
+                output.innerHTML = '';
+                output.innerHTML += `${currentDir}<br>`;
+                termInput.value = '';
+                break;
+            case 'cat':
+                console.log('cat');
+                output.innerHTML = '';
+                output.innerHTML += 'cat: cat: No such file or directory<br>';
+                break;
+            case 'clear':
+                output.innerHTML = '';
+                termInput.value = '';
+                break;
+            case 'help':
+                output.innerHTML = '';
+                output.innerHTML += 'ls - list files in current directory<br>pwd - print current directory<br>cat - concatenate files and print on the standard output<br>clear - clear the terminal<br>help - print this message<br>';
+                termInput.value = '';
+                break;
+            case 'cd':
+                output.innerHTML = '';
+                output.innerHTML += 'not working rn, will fix later';
+                termInput.value = '';
+                break;
+            case 'neofetch':
+                output.innerHTML = '';
+                output.innerHTML += `
+                <pre>
+                ██████████████████  ████████   Sudo@illusions
+                ██████████████████  ████████   -------------------
+                ██████████████████  ████████   OS: Cumjaro Linux x86_64
+                ██████████████████  ████████   Kernel: 5.10.30-1-MANCUMJARO
+                ████████            ████████   Uptime: 1 day, 23 hours, 38 minutes, and 36 seconds
+                ████████  ████████  ████████   Packages: to much(pacman)
+                ████████  ████████  ████████   Shell: bash 5.1.8
+                ████████  ████████  ████████   Terminal: cumsole
+                ████████  ████████  ████████   CPU: shit cum k590-k @ idc@ghz
+                ████████  ████████  ████████   Memory: 16.5G/32G
+                ████████  ████████  ████████
+                ████████  ████████  ████████
+                ████████  ████████  ████████
+                ████████  ████████  ████████
+                </pre>`;
+                break;
+            default:
+                output.innerHTML = '';
+                output.innerHTML += `bash: ${termInput.value}: command not found<br>`;
         }
     }
 });
